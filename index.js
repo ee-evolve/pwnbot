1a8fcb1ed67ec7a435f7b069a1c74537c73d22c3
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-09-25 11:29:24 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+const EMAILS = ['ed.wilson2@hotmail.co.uk'];
+
+const URL = 'https://haveibeenpwned.com/api/v2/breachedaccount/';
+
+const lastWeek = new Date().setDate(new Date().getDate() - 365);
+
+const config = {
+    headers: {
+        'User-Agent': 'Equal Experts Slackbot Checker'
+    }
+};
+
+for (let i = 0; i < EMAILS.length; i++) {
+    let email = EMAILS[i];
+    let path = URL + email;
+    axios.get(path, config)
+        .then(response => {
+            let domains = response.data.filter(event => new Date(event.AddedDate) > lastWeek)
+                .map(event => event.Domain);
+            console.log(domains);
+        })
+        .catch(function () {
+            console.log("No breaches found for " + email);
+    });
+
+}
