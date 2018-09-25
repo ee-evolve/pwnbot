86fdc25b67d966b1d8ed57edf81e0e8fb38732d2
James McNeil <jmcneil@equalexperts.com>
2018-09-25 11:58:31 +0100

    adds slack webhook and posts domains

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const EMAILS = ['ed.wilson2@hotmail.co.uk'];
 
 const URL = 'https://haveibeenpwned.com/api/v2/breachedaccount/';
 
+const SLACK_URL = 'https://hooks.slack.com/services/' + process.env.SLACK_WEBHOOK;
+
 const lastWeek = new Date().setDate(new Date().getDate() - 365);
 
 const config = {
@@ -20,6 +22,8 @@ for (let i = 0; i < EMAILS.length; i++) {
             let domains = response.data.filter(event => new Date(event.AddedDate) > lastWeek)
                 .map(event => event.Domain);
             console.log(domains);
+
+            postSlackMessage(domains)
         })
         .catch(function (e) {
             if (e.response.status === 404) {
@@ -28,5 +32,24 @@ for (let i = 0; i < EMAILS.length; i++) {
                 console.log(e.message)
             }
     });
+}
 
+function postSlackMessage(domains) {
+    let config = {
+        headers: {
+            'Content-type': 'application/json'
+        }
+    };
+
+    let body = {
+        'text': domains.join(", ")
+    };
+
+    axios.post(SLACK_URL, body, config)
+        .then(() => {
+            console.log('Sent')
+        })
+        .catch(function (e) {
+            console.log(e.message)
+    })
 }
